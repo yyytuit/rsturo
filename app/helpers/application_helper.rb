3975191c026851d31cd0909f59c6e3fed62c56f9
吉松寛道 <yyytuit@yahoo.co.jp>
2020-01-03 13:34:46 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
+def yeller(a)
+  b = a.map {|i| i.upcase}
+  puts b.join
+end
+def random_subdomain
+  moji = ('a'..'z').to_a
+  num = (0..9).to_a
+  (moji + num).shuffle[0..7].join
+end
+def string_shuffle(s)
+  s.split('').shuffle.join
+end
+
+def shuffle(s)
+  if s.is_a?(Array)
+    s.join.split('').shuffle.join
+  else
+    s.split('').shuffle.join
+  end
+end
+
+class String
+  def palindrome?
+    self == reverse
+  end
+  def shuffle
+    split('').shuffle.join
+  end
+end
+
+class Word < String             # WordクラスはStringクラスを継承する
+  >>   # 文字列が回文であればtrueを返す
+  >>   def palindrome?
+  >>     self == self.reverse        # selfは文字列自身を表します
+  >>   end
+  >> end
+  => :palindrome?
