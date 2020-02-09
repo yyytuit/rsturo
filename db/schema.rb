387a7637297981960f07ae2deeec262ea24181eb
吉松寛道 <yyytuit@yahoo.co.jp>
2020-02-09 18:37:02 +0900

    11章

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_091953) do
+ActiveRecord::Schema.define(version: 2020_02_08_105821) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 2020_02_08_091953) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
