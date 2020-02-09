ae3206031ec5ea233d29499b02790f784cd70ce7
吉松寛道 <yyytuit@yahoo.co.jp>
2020-02-09 18:53:09 +0900

    heroku修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-cove-17407.herokuapp.com'
+  host = 'https://polar-cove-17407.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
